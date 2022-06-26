31b5502dc067ade0d1fc6fa730aa7c92135b58e5
nikDiq <na.dikarev@northis.ru>
2022-06-26 22:44:56 +0300

    add server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/nik-proj-web/'));
+app.get('/*', function(req,res) {
+    res.sendFile(__dirname+'/dist/nik-proj-web/ index.html');
+});
+app.listen(process.env.PORT || 8080);
